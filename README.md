693564b0a8fcec0881db96f1269a79efdc064a08
Matt Sheppard <msheppard@beta.team>
2024-10-13 08:47:13 -0400

    good simple working version

---
+++ README.md
@@ -0,0 +1,6 @@
+# Morse Pixel
+
+## Button + NeoPixels + MQTT
+* Button publishes MQTT message
+* Light 1 responds to button clicks
+* Light 2 responds to MQTT messages
--- main.py
+++ main.py
@@ -1,60 +1,42 @@
 from umqtt.simple import MQTTClient
-import ubinascii
 
+import ubinascii
 import machine
 import neopixel
-import wifi
 import time
-import random
 import utime
 
+
+# Global variables (don't judge me)
+client = None
+button_value = 0
+
 # PICO pin parameters
-lights = 10
 neopixel_pin = 28
-brake_pin_gpio = 16
 button_pin_gpio = 27
 
 # MQTT Server Parameters
 BROKER = "broker.hivemq.com"
 CLIENT_ID = ubinascii.hexlify(machine.unique_id())
 MQTT_TOPIC = b"com/mampersat/morsepixel"
-TCP_PORT = 1883
-WEBSOCKET_PORT = 8000
-TLS_TCP_PORT = 8883
-TLS_WEBSOCKET_PORT = 8884
-
-# Button/Brake states
-button_value = 0
-tap_counter = 0
-tap_start_time = utime.ticks_ms()
 
 # Initialize the pin and NeoPixel
 button_pin = machine.Pin(button_pin_gpio, machine.Pin.IN, machine.Pin.PULL_UP)
-np = neopixel.NeoPixel(machine.Pin(neopixel_pin), lights)
+np = neopixel.NeoPixel(machine.Pin(neopixel_pin), 2) # TODO when more clients change this
 
 # Callback when a message is received
 def sub_cb(topic, msg):
-    print("Inside callback")
-    print((topic, msg))
-    return True
-    # if the message is pressed, turn on the LED
+    print(f"Received: {msg}")
+
     if msg == b"pressed":
-        print("LED ON")
-        led.fill((255, 255, 255))
-        led.write()
+        np[1] = (255, 0, 0)
+        np.write()
 
+    if msg == b"released":
+        np[1] = (0, 0, 0)
+        np.write()
 
-# Setup MQTT Client and Subscribe
-def mqtt_subscribe():
-    client = MQTTClient(CLIENT_ID, BROKER)
-    client.set_callback(sub_cb)
-    client.connect()
-    client.subscribe(MQTT_TOPIC)
-    print("Connected to %s, subscribed to %s topic" % (BROKER, MQTT_TOPIC))
-    
-    return client
-
-
+# Callback when the button is pressed
 def handle_button_pin_change(pin):
     global mode, button_value
 
@@ -66,24 +48,30 @@ def handle_button_pin_change(pin):
     button_value = value
 
     if value == 0:
-        print("Button pressed")
         np[0] = (255, 0, 0)
         np.write()
-        client.publish(b"com/mampersat/morsepixel", b"pressed")
+        client.publish(MQTT_TOPIC, b"pressed")
+        print("Snet: pressed")
 
     else:
-        print("Button released")
         np[0] = (0, 0, 0)
         np.write()
         client.publish(b"com/mampersat/morsepixel", b"released")
+        print("Sent: released")
 
-button_pin.irq(trigger=machine.Pin.IRQ_RISING | machine.Pin.IRQ_FALLING, handler = handle_button_pin_change)
 
-# Run the MQTT subscription
-# TODO: Make the light blink first
-client = mqtt_subscribe()
+def main():
+    print("Starting main loop")
+    global client
 
-while True:
-    # think about the future
-    print("Hello, World!")
-    time.sleep(1)
+    button_pin.irq(trigger=machine.Pin.IRQ_RISING | machine.Pin.IRQ_FALLING, handler = handle_button_pin_change)
+
+    client = MQTTClient(CLIENT_ID, BROKER)
+    client.set_callback(sub_cb)
+    client.connect()
+    client.subscribe(MQTT_TOPIC)
+    while True:
+        # Blocking wait for message
+        client.wait_msg()
+
+main()            
