355a440d8f3f4cde72688476684bb0785d746e71
Matt Sheppard <msheppard@beta.team>
2024-10-15 09:22:16 -0400

    json and colors

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from umqtt.simple import MQTTClient
 import ubinascii
 import machine
 import neopixel
+import json
 import utime
 
 import wifi
@@ -15,26 +16,47 @@ button_value = 0
 neopixel_pin = 28
 button_pin_gpio = 27
 
+
 # MQTT Server Parameters
 BROKER = "broker.hivemq.com"
 CLIENT_ID = ubinascii.hexlify(machine.unique_id())
 MQTT_TOPIC = b"com/mampersat/morsepixel"
-CONTROLLED_PIXEL = 0
+
+# The unique name of the pixel we are controlling
+CONTROLLED_PIXEL = "J-38" # the number on the morse code key device
+LOCAL_COLOR = (10, 20, 30) # the color of the pixel when the button is pressed locally, a light blue
+
+# The list of pixels we want to display
+PIXELS = [CONTROLLED_PIXEL, "eilly"] # map pixels to neopixel locations starting at position 1
 
 # Initialize the pin and NeoPixel
 button_pin = machine.Pin(button_pin_gpio, machine.Pin.IN, machine.Pin.PULL_UP)
-np = neopixel.NeoPixel(machine.Pin(neopixel_pin), 2) # TODO when more clients change this
+np = neopixel.NeoPixel(machine.Pin(neopixel_pin), 10) # TODO if you have more than 10 pixels, change this
 
 # Callback when a message is received
 def sub_cb(topic, msg):
-    print(f"Received: {msg}")
 
-    if msg == b"pressed":
-        np[1] = (255, 0, 0)
+    msg_json = json.loads(msg)
+    pixel = msg_json['pixel']
+
+    if pixel not in PIXELS:
+        print(f"Invalid pixel: {pixel}")
+        return
+    
+    location = PIXELS.index(pixel) + 1 # 1-based index, 0 index is control pixel
+    state = msg_json['state']
+    
+    color = (255, 0, 0)
+    if 'color' in msg_json:
+        color = msg_json['color']
+        print(f"Setting pixel {pixel} to {color}")
+
+    if state == "pressed":
+        np[location] = tuple(color)
         np.write()
 
-    if msg == b"released":
-        np[1] = (0, 0, 0)
+    if state == "released":
+        np[location] = (0, 0, 0)
         np.write()
 
 # Callback when the button is pressed
@@ -49,15 +71,17 @@ def handle_button_pin_change(pin):
     button_value = value
 
     if value == 0:
-        np[0] = (255, 0, 0)
+        np[0] = LOCAL_COLOR
         np.write()
-        client.publish(MQTT_TOPIC, b"pressed")
+        message = {"pixel": CONTROLLED_PIXEL, "state": "pressed", "color": LOCAL_COLOR}
+        client.publish(MQTT_TOPIC, json.dumps(message))
         print("Sent: pressed")
 
     else:
         np[0] = (0, 0, 0)
         np.write()
-        client.publish(b"com/mampersat/morsepixel", b"released")
+        message = {"pixel": CONTROLLED_PIXEL, "state": "released"}
+        client.publish(MQTT_TOPIC, json.dumps(message))
         print("Sent: released")
 
 
@@ -71,8 +95,29 @@ def main():
     client.set_callback(sub_cb)
     client.connect()
     client.subscribe(MQTT_TOPIC)
+
+    last_connect_time = utime.time()
+
     while True:
-        # Blocking wait for message
-        client.wait_msg()
+        # Blocking wait for message, swallowing exceptions
+        try:
+            client.check_msg()
+        except Exception as e:
+            print(f"Error: {e}")
+
+        # Reconnect to the broker every 10min
+        if utime.time() - last_connect_time > 600:
+            print("Reconnecting to broker")
+
+            # attempt to connect with exponential back off
+            for i in range(32): # 100 years... come find me in 100yrs and complain if this is a problem
+                try:
+                    client.connect()
+                    client.subscribe(MQTT_TOPIC)
+                    last_connect_time = utime.time()
+                    break
+                except:
+                    print(f"Failed to connect. Retrying in {2**i} seconds")
+                    utime.sleep(2**i)
 
 main()
--- morse-pixel.py
+++ morse-pixel.py
@@ -3,15 +3,27 @@ from pynput import keyboard
 import threading
 import paho.mqtt.client as mqtt
 import uuid
+import json
 
 # Initialize the spacebar state
 spacebar_pressed = False
-remote_pressed = False
+remote_pressed = []
 
 # MQTT Server Parameters
 BROKER = "broker.hivemq.com"
 CLIENT_ID = uuid.getnode() # unique ID for this client
 MQTT_TOPIC = "com/mampersat/morsepixel"
+
+# The unique name of the pixel we are controlling
+CONTROLLED_PIXEL = "eilly" # the number on the morse code key device
+LOCAL_COLOR = (30, 20, 10) # the color of the pixel when the button is pressed locally, a light blue
+
+# The list of pixels we want to display
+pixels = {
+    CONTROLLED_PIXEL: 0,
+    "J-38": 0
+}
+
 mqtt_status = 'unconnected'
 
 def on_connect(client, userdata, flags, reason_code, properties):
@@ -21,32 +33,45 @@ def on_connect(client, userdata, flags, reason_code, properties):
     client.subscribe("$SYS/#")
 
 def on_message(client, userdata, msg):
-    global remote_pressed
-    # print(msg.topic+" "+str(msg.payload))
+    global remote_pressed, mqtt_status
+    mqtt_status = msg.payload
+    return
 
-    if msg.payload == b"pressed":
+    msg_json = json.loads(msg.payload)
+    pixel = msg_json['pixel']
+
+    if pixel not in pixels:
+        print(f"Invalid pixel: {pixel}")
+        return
+    
+    location = pixels.index(pixel) + 1 # 1-based index, 0 index is control pixel
+    state = msg_json['state']
+
+    if state == b"pressed":
         remote_pressed = True
 
-    if msg.payload == b"released":
+    if state == b"released":
         remote_pressed = False
 
 def on_press(key):
     global spacebar_pressed, client
     if key == keyboard.Key.space:
         spacebar_pressed = True
-        client.publish(MQTT_TOPIC, b"pressed")
-
+        message = {"pixel": CONTROLLED_PIXEL, "state": "pressed", "color": LOCAL_COLOR}
+        client.publish(MQTT_TOPIC, json.dumps(message))
 
 def on_release(key):
     global spacebar_pressed
     if key == keyboard.Key.space:
         spacebar_pressed = False
-        client.publish(MQTT_TOPIC, b"released")
+        message = {"pixel": CONTROLLED_PIXEL, "state": "released"}
+        client.publish(MQTT_TOPIC, json.dumps(message))
 
 def start_key_listener():
     # Start the pynput listener in a separate thread
     with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
         listener.join()
+
 
 def main(stdscr):
     global spacebar_pressed, remote_pressed, mqtt_status, client
