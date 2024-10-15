68ab3e1eb7311da0bc952b782733e766d3f5b336
Matt Sheppard <msheppard@beta.team>
2024-10-15 15:08:31 -0400

    make password change more obvious

--- wifi.py
+++ wifi.py
@@ -5,7 +5,7 @@ time.sleep(1) # Give the device a hot second to be ready for wifi
 
 sta_if = network.WLAN(network.STA_IF)
 sta_if.active(True)
-sta_if.connect('ShArVa', 'end dirt people main zero')
+sta_if.connect('ShArVa', 'your wifi password here')
 
 time.sleep(1) # better would be a callback somewhere
 print('wifi connected')
